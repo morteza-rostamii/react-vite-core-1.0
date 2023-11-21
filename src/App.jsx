7f1eb8ec7821992e9a5be073d128dcee119e2557
rorteza rostami <morteza9rostami@gmail.com>
2023-11-21 07:15:34 -0600

    react app setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Morteza from '@/views/components/Morteza'
+import {Button} from '@mantine/core'
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div
+    className='
+    bg-green-100
+    '
+    >
+      <Routes>
+        <Route
+        path=''
+        >
+          
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/views/layouts/MainLay.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const MainLay = () => {
+  return (
+    <div>
+      <Header/>
+      MainLay
+    </div>
+  )
+}
+
+export default MainLay
